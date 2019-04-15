45b9eb2096c8828c7f5ffc68e9c879d94e446946
sejinkay <sejinkay@gmail.com>
2019-04-15 16:26:17 +0100

    added cat picture

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 ruby '2.6.0'
 
   gem 'sinatra'
+  gem 'shotgun'
 
 group :test, :development do
   gem 'rspec'
--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   'Hello!'
+end
+
+get '/secret' do
+  'Heeeey....'
+end
+
+get '/halloween' do
+  'boooooooo'
+end
+
+get '/cat' do
+"<div style = 'border: dotted red;'>
+<img src = http://bit.ly/1eze8aE></div>"
 end
