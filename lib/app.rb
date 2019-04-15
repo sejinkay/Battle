2701dca6f62dc6cc6c99bad5e308eada81b4e9c9
MaxRobertsDear <maxhroberts@gmail.com>
2019-04-15 17:32:15 +0100

    make webpage interactive by adding params

--- lib/app.rb
+++ lib/app.rb
@@ -13,7 +13,13 @@ get '/halloween' do
   'boooooooo'
 end
 
-get '/cat' do
+get '/random-cat' do
 @cat_name = ["Amigo", "Oscar", "Viking"].sample
 erb(:index)
 end
+
+get '/named-cat' do
+  p params 
+  @cat_name = params[:name]
+  erb(:index)
+  end
