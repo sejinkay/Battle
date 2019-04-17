b9c595a23b23d9049654e49f408ef61cbc01bffe
sejinkay <sejinkay@gmail.com>
2019-04-17 11:42:55 +0100

    added infrastructure test

--- spec/features/infrastructure_test_spec.rb
+++ spec/features/infrastructure_test_spec.rb
@@ -1,1 +1,8 @@
-# require 'app'
+# require 'app' <= no need for require?
+
+feature 'Testing infrastructure' do
+ scenario 'checks what homepage says' do
+   visit('/')
+   expect(page).to have_content "Testing infrastructure working!"
+ end
+end
